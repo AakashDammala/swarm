a44d56078f6c33abd8a19dee26846c90c1a87814
Aakash Dammala <dammala.aakash@gmail.com>
2025-12-06 23:48:56 -0500

    Task 2.7 - Completed FSM model 1. It has below states, and defines the state transitions betweeen them   FIND_OBJECT   APPROACH_OBJECT   GRASP_OBJECT   MOVE_HOME   MOVE_OUT   RELEASE_OBJECT

--- src/swarm_model/include/swarm_model/robot_fsm.hpp
+++ src/swarm_model/include/swarm_model/robot_fsm.hpp
@@ -13,33 +13,39 @@ namespace swarm_model
 
 enum class State
 {
-  IDLE,
-  SEARCHING,
-  COLLECTING,
-  DUMPING,
-  AVOIDING
+  FIND_OBJECT,
+  APPROACH_OBJECT,
+  GRASP_OBJECT,
+  MOVE_HOME,
+  MOVE_OUT,
+  RELEASE_OBJECT
 };
 
 class RobotFSM
 {
-public:
+ public:
   RobotFSM();
   void update();
   State get_state() const;
 
   // Inputs from sensors (setters)
   void set_object_detected(bool detected);
-  void set_obstacle_detected(bool detected);
+  void set_distance_to_object(double distance);
+  void set_at_home(bool at_home);
+  void set_at_drop(bool at_drop);
+  void set_action_complete(bool complete);  // Generic flag for discrete actions (grasp, release)
 
-private:
+ private:
   State current_state_;
   bool object_detected_;
-  bool obstacle_detected_;
+  double distance_to_object_;
+  bool at_home_;
+  bool at_drop_;
+  bool action_complete_;
+  bool holding_object_;  // Internal state to track if we have the object
 
-  void search();
-  void collect();
-  void dump();
-  void avoid();
+  // Remove individual behavior methods as they are handled in update() state transitions or
+  // Controller
 };
 
 }  // namespace swarm_model
